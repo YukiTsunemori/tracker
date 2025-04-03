c3b1349215bc86dc546e3be5437ded03d322584b
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-04-03 17:40:46 +0900

    Create a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # callback関数 => ユーザーをDBへ保存する前にemail属性を強制的に小文字へ変換する。
+  # selfは現在のユーザーを指す。
+  before_save { self.email = email.downcase } 
+
+  validates :name, presence: true,  length: { maximum: 50 }
+  #　正規表現を定数へ代入。以下、Emailのvalidationでformatで使用する。
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+  # => 1. ハッシュ化したパスワードをDB内のカラム、password_digestに保存できる。
+  # => 2. ２つの仮想的な属性(passwordとpassword_confirmation)が使える。また存在性と値が一致するかどうかのバリデーションも追加される。
+  # => 3. authenticateメソッドが使える。一致しない場合はfalseを返す。
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
@@ -9,6 +10,7 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+
 
     # Add more helper methods to be used by all tests here...
   end
