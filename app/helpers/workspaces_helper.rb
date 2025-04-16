7ca6c2372e3f0b160bd0066fd685f4f03143fe3f
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-04-17 00:57:49 +0900

    Add comment to exist? method

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -7,6 +7,8 @@ module WorkspacesHelper
   def exist?
     @workspace_exist ||= Workspace.where(user_id: current_user_id)
     return @workspace_exist.exists?
+    # exists?メソッドはSQLが発行されるため、空の配列オブジェクトを返す場合にのみ使える。
+    # シンプルな空の配列にはexists?メソッドは使えない（SQL文ではない）のでメソッドエラーとなる。
   end
 
 
